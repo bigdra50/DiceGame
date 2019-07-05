d085aff8efb50237f6399170d7e1525040d4c8f8
bigdra50 <ryu1998tea@gmail.com>
2019-07-05 16:54:53 +0900

    add something

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -9,17 +9,18 @@ namespace DiceGame
         public static void Main(string[] args)
         {
             var seed = new System.Random((int)DateTime.Now.Ticks);
-            System.Console.WriteLine("Rolling the dice...");
+            Console.WriteLine("Rolling the dice...");
 
             var dice1 = seed.Next(2, 7);
             var dice2 = seed.Next(2, 7);
             var total = dice1 + dice2;
 
-            System.Console.WriteLine($"Die 1: {dice1}");
-            System.Console.WriteLine($"Die 2: {dice2}");
+            Console.WriteLine($"Die 1: {dice1}");
+            Console.WriteLine($"Die 2: {dice2}");
             
             
-            System.Console.WriteLine($"Total value: {total}");
+            Console.WriteLine($"Total value: {total}");
+            Console.WriteLine(total > 7 ? "You won": "You lost");
         }
     }
 }
